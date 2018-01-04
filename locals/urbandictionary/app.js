45ea936063b7970a057c7c704bf465ef2185a0ae
Oleg Kertanov <okertanov@gmail.com>
2018-01-04 16:18:52 +0200

    urbandictionary update

--- locals/urbandictionary/app.js
+++ locals/urbandictionary/app.js
@@ -1,16 +1,37 @@
-$(document).on('click', nextSlide);
+//
+// app.js
+//
 
-window.setInterval(nextSlide, 10000);
+var randomUrbanTerms = undefined;
 
-function nextSlide() {
-  console.log($('.active + .slide').length);
+let nextSlide = function() {
+  console.log('Active:', $('.active + .slide').length);
 
   if ($('.active + .slide').length > 0) {
-    $('.active + .slide').addClass('active');
+    $('.active + .slide').not('.loading').addClass('active');
     $($('.active')[0]).removeClass('active');
   }
   else {
     $('.active').removeClass('active');
-    $('.slide:nth-child(1)').addClass('active');
+    $('.slide:nth-child(2)').addClass('active');
   }
-}
+};
+
+let fetchRandomUrbanTerms = function() {
+    if (!randomUrbanTerms) {
+        var jqxhr = $.get('http://api.urbandictionary.com/v0/random')
+            .done(function(d) {
+                console.log('Done:', d.list.length);
+            })
+            .fail(function(e) {
+                console.warn('Error:', e);
+            })
+            .always(function(p) {
+                console.log('Finished:', p);
+            });
+    }
+};
+
+$(document).on('click', nextSlide);
+
+window.setInterval(nextSlide, 10000);
