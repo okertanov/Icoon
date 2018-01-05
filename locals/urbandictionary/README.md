fa669a9d1d936ec91240dc18ee3ab1b5d0b497b7
Oleg Kertanov <okertanov@gmail.com>
2018-01-05 18:16:50 +0200

    Save feature added

--- locals/urbandictionary/README.md
+++ locals/urbandictionary/README.md
@@ -17,3 +17,7 @@ https://keyvalue.xyz/
 https://api.keyvalue.xyz/6221fe27/Icoon
 
 https://stackoverflow.com/questions/9024819/free-cloud-data-stores-that-use-get-post
+
+https://console.cloud.google.com/datastore/welcome?project=icoon-web
+
+https://admin.pubnub.com/#/user/431199/account/431163/app/35177891/
--- locals/urbandictionary/app.js
+++ locals/urbandictionary/app.js
@@ -14,7 +14,7 @@ let nextSlide = () => {
         recurringTimeoutID = undefined;
     }
 
-    if (!randomUrbanTerms) {
+    if (randomUrbanTerms) {
         console.log('Active:', $('.active + .slide').length);
 
         if ($('.active + .slide').length > 0) {
@@ -39,31 +39,32 @@ let startAutoSlide = (timeout) => {
 
 let fetchRandomUrbanTerms = () => {
     const startSlideTimeout = 3000;
-    if (!randomUrbanTerms) {
-        var jqxhr = $.get('http://api.urbandictionary.com/v0/random')
-            .done(function(d) {
-                console.log('Done:', d.list.length);
-                $('.active + .slide + .loading').removeClass('loading-animation');
-                $('.slide').not('.loading').each(function(i, slide) {
-                    console.log('Iterating:', i, slide);
-                    let term = d.list[i];
-                    if (term) {
-                        $(slide).find('.panel .top').html(term.word);
-                        $(slide).find('.panel .top').data('back', term.word);
-                        $(slide).find('.panel .bottom').html(term.word);
-                        $(slide).find('.panel .bottom').data('back', term.word);
-                        $(slide).find('.center h1').html(term.definition);
-                    }
-                });
-                startAutoSlide(startSlideTimeout);
-            })
-            .fail(function(e) {
-                console.warn('Error:', e);
-            })
-            .always(function(p) {
-                console.log('Finished:', p);
+
+    var jqxhr = $.get('http://api.urbandictionary.com/v0/random')
+        .done(function(d) {
+            console.log('Done:', d.list.length);
+
+            randomUrbanTerms = d.list;
+
+            $('.active + .slide + .loading').removeClass('loading-animation');
+            $('.slide').not('.loading').each(function(i, slide) {
+                let term = d.list[i];
+                if (term) {
+                    $(slide).find('.panel .top').html(term.word);
+                    $(slide).find('.panel .top').data('back', term.word);
+                    $(slide).find('.panel .bottom').html(term.word);
+                    $(slide).find('.panel .bottom').data('back', term.word);
+                    $(slide).find('.center h1').html(term.definition);
+                }
             });
-    }
+            startAutoSlide(startSlideTimeout);
+        })
+        .fail(function(e) {
+            console.warn('Error:', e);
+        })
+        .always(function(p) {
+            console.log('Finished:', p);
+        });
 };
 
 let performFullReload = () => {
@@ -85,7 +86,20 @@ let performFullReload = () => {
 };
 
 let saveCurrentTerm = () => {
-    
+    console.log('Saving current term...');
+
+    if (!randomUrbanTerms) {
+        return;
+    }
+
+    $('.slide').not('.loading').each(function(i, slide) {
+        if ($(slide).hasClass('active')) {
+            let term = randomUrbanTerms[i];
+            if (term) {
+                publishToBaaS(term);
+            }
+        }
+    });
 };
 
 const showOverlayTimeout = 2000;
@@ -145,9 +159,112 @@ let keyCommandHandler = (e) => {
     }
 };
 
+var pubnub = undefined;
+
+let initBaaS = () => {
+    pubnub = new PubNub({
+        publishKey: "pub-c-8a217905-fba2-49fb-8172-6d098d2c064c",
+        subscribeKey: "sub-c-954f25ae-f22a-11e7-acf8-26f7716e5467",
+        ssl: false
+    });
+
+    pubnub.addListener({
+        message: function(m) {
+            // handle message
+            var actualChannel = m.actualChannel;
+            var channelName = m.channel; // The channel for which the message belongs
+            var msg = m.message; // The Payload
+            var publisher = m.publisher;
+            var subscribedChannel = m.subscribedChannel;
+            var channelGroup = m.subscription; // The channel group or wildcard subscription match (if exists)
+            var pubTT = m.timetoken; // Publish timetoken
+
+            console.log('PubNub message:', m);
+        },
+        presence: function(p) {
+            // handle presence
+            var action = p.action; // Can be join, leave, state-change or timeout
+            var channelName = p.channel; // The channel for which the message belongs
+            var channelGroup = p.subscription; //  The channel group or wildcard subscription match (if exists)
+            var presenceEventTime = p.timestamp; // Presence event timetoken
+            var status = p.status; // 200
+            var message = p.message; // OK
+            var service = p.service; // service
+            var uuids = p.uuids;  // UUIDs of users who are connected with the channel with their state
+            var occupancy = p.occupancy; // No. of users connected with the channel
+
+            console.log('PubNub presence:', p);
+        },
+        status: function(s) {
+            // handle status
+            var category = s.category; // PNConnectedCategory
+            var operation = s.operation; // PNSubscribeOperation
+            var affectedChannels = s.affectedChannels; // The channels affected in the operation, of type array.
+            var subscribedChannels = s.subscribedChannels; // All the current subscribed channels, of type array.
+            var affectedChannelGroups = s.affectedChannelGroups; // The channel groups affected in the operation, of type array.
+            var lastTimetoken = s.lastTimetoken; // The last timetoken used in the subscribe request, of type long.
+            var currentTimetoken = s.currentTimetoken; // The current timetoken fetched in the subscribe response, which is going to be used in the next request, of type long.
+
+            console.log('PubNub status:', s);
+        }
+    });
+
+    pubnub.time((status, response) => {
+        console.log('PubNub time', status.error, response.timetoken);
+    });
+
+    pubnub.subscribe({ channels: ['icoon_channel'] });
+
+    pubnub.history({
+        channel: 'icoon_channel',
+        reverse: false,
+        count: 1000,
+        stringifiedTimeToken: true
+    },
+    (status, response) => {
+        console.log('PubNub history', status, response);
+    });
+};
+
+let finalizeBaaS = () => {
+    if (!pubnub) {
+        return;
+    }
+
+    pubnub.unsubscribe({ channels: ['icoon_channel'] });
+
+    var existingListener = {
+        message: function() {}
+    };
+
+    pubnub.removeListener(existingListener);
+};
+
+let publishToBaaS = (payload) => {
+    if (!pubnub) {
+        return;
+    }
+
+    pubnub.publish({
+            message: { such: payload },
+            channel: 'icoon_channel',
+            sendByPost: false,
+            storeInHistory: true,
+            meta: { "session": "debug" }
+        },
+        function (status, response) {
+            console.log('PubNub publish', status.error, response.timetoken);
+        }
+    );
+};
+
+// Event Listeners
 $(document).on('click', nextSlide);
 $(document).on('mousemove', showHintAt);
 $(document).on('keypress', keyCommandHandler);
 
 // Initial Bootstrap
 performFullReload();
+
+// PubNub Infrastructure
+initBaaS();
