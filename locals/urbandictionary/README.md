858ce55e48f2f6172b59dba4105b2e6e34a9af5a
Oleg Kertanov <okertanov@gmail.com>
2018-01-05 16:34:59 +0200

    Improvements: help, key commands, reload

--- locals/urbandictionary/README.md
+++ locals/urbandictionary/README.md
@@ -10,3 +10,10 @@ Text Effects
 ------------
 https://codemyui.com/slides-text-splitting-middle/
 https://codepen.io/nathantaylor/pen/ZLRLvZ
+
+Backends
+--------
+https://keyvalue.xyz/
+https://api.keyvalue.xyz/6221fe27/Icoon
+
+https://stackoverflow.com/questions/9024819/free-cloud-data-stores-that-use-get-post
--- locals/urbandictionary/app.js
+++ locals/urbandictionary/app.js
@@ -2,24 +2,43 @@
 // app.js
 //
 
+const nextSlideInterval = 20000;
+
 var randomUrbanTerms = undefined;
+var initialTimeoutID = undefined;
+var recurringTimeoutID = undefined;
 
 let nextSlide = () => {
-  console.log('Active:', $('.active + .slide').length);
+    if (recurringTimeoutID) {
+        window.clearTimeout(recurringTimeoutID);
+        recurringTimeoutID = undefined;
+    }
 
-  if ($('.active + .slide').length > 0) {
-    $('.active + .slide').not('.loading').addClass('active');
-    $($('.active')[0]).removeClass('active');
-  }
-  else {
-    $('.active').removeClass('active');
-    $('.slide:nth-child(2)').addClass('active');
-  }
+    if (!randomUrbanTerms) {
+        console.log('Active:', $('.active + .slide').length);
+
+        if ($('.active + .slide').length > 0) {
+            $('.active + .slide').not('.loading').addClass('active');
+            $($('.active')[0]).removeClass('active');
+        }
+        else {
+            $('.active').removeClass('active');
+            $('.slide:nth-child(2)').addClass('active');
+        }
+    }
+    else {
+        console.warn('There are no random Urban terms there.');
+    }
+
+    recurringTimeoutID = window.setTimeout(nextSlide, nextSlideInterval);
+};
+
+let startAutoSlide = (timeout) => {
+    initialTimeoutID = window.setTimeout(nextSlide, timeout);
 };
 
 let fetchRandomUrbanTerms = () => {
-    const startSlideInterval = 3000;
-    const nextSlideInterval = 20000;
+    const startSlideTimeout = 3000;
     if (!randomUrbanTerms) {
         var jqxhr = $.get('http://api.urbandictionary.com/v0/random')
             .done(function(d) {
@@ -36,7 +55,7 @@ let fetchRandomUrbanTerms = () => {
                         $(slide).find('.center h1').html(term.definition);
                     }
                 });
-                window.setTimeout(() => { nextSlide(); window.setInterval(nextSlide, nextSlideInterval); }, startSlideInterval);
+                startAutoSlide(startSlideTimeout);
             })
             .fail(function(e) {
                 console.warn('Error:', e);
@@ -47,6 +66,88 @@ let fetchRandomUrbanTerms = () => {
     }
 };
 
-$(document).on('click', nextSlide);
+let performFullReload = () => {
+    if (recurringTimeoutID) {
+        window.clearTimeout(recurringTimeoutID);
+    }
+    if (initialTimeoutID) {
+        window.clearTimeout(initialTimeoutID);
+    }
+    randomUrbanTerms = undefined;
 
-fetchRandomUrbanTerms();
+    if ($('.active + .slide').length > 0) {
+        $($('.active')[0]).removeClass('active');
+    }
+    $('.slide:nth-child(1)').addClass('active');
+    $('.slide:nth-child(1)').addClass('loading-animation');
+
+    fetchRandomUrbanTerms();
+};
+
+let saveCurrentTerm = () => {
+    
+};
+
+const showOverlayTimeout = 2000;
+var hintTimeoutId = undefined;
+
+let hideOverlay = (overlay, timeoutId) => {
+    let visible = $(overlay).is(':visible');
+    if (visible) {
+        $(overlay).fadeTo(1000, 0, () => { $(overlay).css('display', 'none'); });
+    }
+    if (timeoutId) {
+        window.clearTimeout(timeoutId);
+    }
+};
+
+let showHintAt = (e) => {
+    // console.log(e);
+
+    $('#hint-overlay').css({left: event.clientX, top: event.clientY, position:'absolute'});
+
+    let visible = $('#hint-overlay').is(':visible');
+    if (!visible) {
+        $('#hint-overlay').fadeTo(1000, 0.5, () => { $('#hint-overlay').css('display', 'inline-block'); });
+    }
+    if (hintTimeoutId) {
+        window.clearTimeout(hintTimeoutId);
+    }
+    hintTimeoutId = window.setTimeout(() => hideOverlay('#hint-overlay', hintTimeoutId), showOverlayTimeout);
+};
+
+let keyCommandHandler = (e) => {
+    // console.log(e);
+
+    let key = e.key.toLowerCase();
+    var handled = false;
+
+    switch (key) {
+        case 'n':
+            nextSlide();
+            handled = true;
+        break;
+        case 'r':
+            performFullReload();
+            handled = true;
+        break;
+        case 's':
+            saveCurrentTerm();
+            handled = true;
+        break;
+        default:
+            handled = false;
+        break;
+    }
+
+    if (handled) {
+        e.preventDefault();
+    }
+};
+
+$(document).on('click', nextSlide);
+$(document).on('mousemove', showHintAt);
+$(document).on('keypress', keyCommandHandler);
+
+// Initial Bootstrap
+performFullReload();
